88bd905b793a5ca78f298b74ebb0861f05a30db8
Jakub Pelák <jpelak@gmail.com>
2013-02-27 09:27:36 +0100

    proper caching

--- publisher/cfg/env.php
+++ publisher/cfg/env.php
@@ -6,27 +6,32 @@ class Env extends \Lemmon\Environment
 {
 
 
-	protected function __init()
-	{
-		self::setDev();
-		
-		// DEVELOPMENT
-		if (self::isDev())
-		{
-			// template dev environment
-			\Lemmon\Template\Template::setDefaultEnvironment([
-				'auto_reload' => true,
-				'debug'       => true,
-			]);
-		}
-		// PRODUCTION
-		else
-		{
-			// TODO
-			die('EnvBase/ PRODUCTION');
-		}
+    protected function __init()
+    {
+        self::setDev();
+        
+        // DEVELOPMENT
+        if (self::isDev())
+        {
+            // template dev environment
+            \Lemmon\Template\Template::setDefaultEnvironment([
+                'auto_reload' => true,
+                'debug'       => true,
+                'cache'       => BASE_DIR . '/cache/tpl',
+            ]);
+        }
+        // PRODUCTION
+        else
+        {
+            // TODO
+            die('EnvBase/ PRODUCTION');
+            // template dev environment
+            \Lemmon\Template\Template::setDefaultEnvironment([
+                'cache' => BASE_DIR . '/cache/tpl',
+            ]);
+        }
 
-		// uploads
-		\Lemmon\Model\Schema::setDefaultUploadDir(BASE_DIR . '/user/uploads');
-	}
+        // uploads
+        \Lemmon\Model\Schema::setDefaultUploadDir(BASE_DIR . '/user/uploads');
+    }
 }
